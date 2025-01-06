44f6d8ed1cf44aa61e8f3d13ccd9603a22241d8f
scrabbleronix <rawbokcat2010@gmail.com>
2025-01-06 15:56:46 -0800

    Update script.js

--- script.js
+++ script.js
@@ -359,7 +359,7 @@ function playSound(type) {
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
     navigator.serviceWorker
-      .register("/service-worker.js")
+      .register("/Driflux/service-worker.js")
       .then((registration) => {
         console.log("Service Worker registered with scope:", registration.scope);
 
